25576c39662f01d57848c8a66fb276bc761e8c36
neleon10 <cvn45@hotmail.com>
2023-06-08 19:33:07 +0200

    nuevo proyecto java

---
+++ bootcampJava/src/Main.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Carlos";
+        Integer nroEquipo = 1;
+        String text = "Hola soy " + name + " perteneciente al equipo número " + nroEquipo;
+        System.out.println(text);
+
+    }
+}
